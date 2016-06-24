fec5f422cbaa09261bd93a67b548a509bdb70149
WELLINGTON <wtonfds@hotmail.com>
2016-06-24 18:14:02 -0300

    Merge branch 'master' of https://github.com/Hallessandro/SISPROD
    # Conflicts:
    #	.idea/SISPROD.iml

--- appprod/views.py
+++ appprod/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from appprod.models import *
+from appprod.models import*
 
 
 def home(request):
     return render(request,'base.html')
 
-def exibirPrestador(request,id):
-        prestador = Prestador_sevico.objects.get(id=id)
+def exibirPrestador(request):
+        prestador = Prestador_servico.objects.all()
         return render(request, 'exibirprestador.html', {'prestador': prestador})
+
+
+def exibirMateriaPrima(request, descricao):
+    materia = Materia_prima.objects.get(descricao=descricao)
+    return render(request, 'exibirMateria.html', {'materia': materia})
 
