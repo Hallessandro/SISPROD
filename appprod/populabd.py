bf429a4c45118224637b1aae2386bab05a542fae
ed9trabalho <ed9trabalho@gmail.com>
2016-06-23 22:32:49 -0300

    Merge branch 'master' of https://github.com/ed9trabalho/gestaoprojetos
    Conflicts:
    	.idea/vcs.xml
    	appprojetos/consultas.py
    	appprojetos/populabd.py

--- appprod/populabd.py
+++ appprod/populabd.py
@@ -1,4 +1,12 @@
 from django.db import transaction,IntegrityError
-from SISPROD.models import *
 
-cargo1 = Cargo
+from appprod.models import Cargo
+
+cargo1 = Cargo(descricao='vendedor',salario=880)
+cargo2 = Cargo(descricao='produtor',salario=1180)
+
+cargo1.save()
+cargo2.save()
+
+
+
