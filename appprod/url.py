a60d63953ab1919cf98a7f3b110f31d50af2e458
WELLINGTON <wtonfds@hotmail.com>
2016-06-24 14:32:22 -0300

    Merge branch 'master' of https://github.com/ed9trabalho/gestaoprojetos
    terminando algumas variações

---
+++ appprod/url.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns,include,url
+from appprod.views import *
+
+urlpatterns=[
+    url(r'^$',home,name='home'),
+    url(r'^produtos/$',exibirPrestador(),name='produtos'),
+]
--- appprod/views.py
+++ appprod/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from appprod.models import *
 
-# Create your views here.
+
+def home(request):
+    return render(request,'base.html')
+
+def exibirPrestador(request,id):
+        prestador = Prestador_sevico.objects.get(id=id)
+        return render(request, 'exibirprestador.html', {'prestador': prestador})
+
