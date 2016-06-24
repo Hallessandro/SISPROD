8296fe1cf88ac987e3782d1faf553c58b84c3337
WELLINGTON <wtonfds@hotmail.com>
2016-06-24 15:26:15 -0300

    Merge branch 'master' of https://github.com/Hallessandro/SISPROD
    # Conflicts:
    #	.idea/SISPROD.iml

--- SISPROD/urls.py
+++ SISPROD/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from appprod.views import *
 
-urlpatterns = [
+urlpatterns=[
     url(r'^admin/', admin.site.urls),
+    url(r'^$',home,name='home'),
+    url(r'^produtos/$',exibirPrestador(),name='produtos'),
 ]
--- appprod/url.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns,include,url
-from appprod.views import *
-
-urlpatterns=[
-    url(r'^$',home,name='home'),
-    url(r'^produtos/$',exibirPrestador(),name='produtos'),
-]
